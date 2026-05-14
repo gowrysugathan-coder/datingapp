f4c260f69796be8c21cc978560bbab324be6ee92
gowry189 <gowrysugathan18@gmail.com>
2026-05-14 13:39:55 +0530

    end of section 4

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,15 +3,17 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(AppDbContext context) : BaseApiController
+public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await EmailExists(registerDto.Email))
         {
@@ -27,10 +29,10 @@ public class AccountController(AppDbContext context) : BaseApiController
         };
         context.Users.Add(user);
         await context.SaveChangesAsync();
-        return user;
+        return user.ToDto(tokenService);
     }
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login([FromBody] LoginDto loginDto)
+    public async Task<ActionResult<UserDto>> Login([FromBody] LoginDto loginDto)
     {
         var user = await context.Users.FirstOrDefaultAsync(x => x.Email == loginDto.Email);
         if(user == null) return Unauthorized("Invalid Email Address");
@@ -42,7 +44,7 @@ public class AccountController(AppDbContext context) : BaseApiController
             if(computedHash[i] != user.PasswordHash[i])
             return Unauthorized("Invalid password");
         }
-        return user;
+        return user.ToDto(tokenService); 
     }
 
     private async Task<bool> EmailExists(string email)
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,12 @@
+using System.Text;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens.Experimental;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +18,24 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService,TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("Token key not found - program.cs");
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 var app = builder.Build();
 app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200","http://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
